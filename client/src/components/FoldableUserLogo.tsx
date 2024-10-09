15608302dc2f4f00d9e4d79fb6d7de004903daaa
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2024-10-10 00:27:38 +0200

    make user logo menu even better

--- client/src/components/FoldableUserLogo.tsx
+++ client/src/components/FoldableUserLogo.tsx
@@ -1,22 +1,25 @@
 import { AnimatePresence, motion } from "framer-motion";
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, useCallback } from "react";
 import { useAuth } from "src/context/auth.context";
 import { assembleImageUrl } from "./MapWithFriendMarkers";
 
-export const FoldableUserLogo = () => {
-  const { user, logOutUser } = useAuth();
-  const [open, setOpen] = useState(false);
-  const menuRef = useRef<HTMLDivElement>(null!);
-
-  const toggleOpen = () => {
-    setOpen((prev) => !prev);
-  };
-
-  const handleClickOutside = (event: MouseEvent) => {
-    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
-      setOpen(false);
-    }
-  };
+export const useCloseWhenClickedOutside = ({
+  ref,
+  open,
+  setOpen,
+}: {
+  ref: React.RefObject<HTMLElement>;
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const handleClickOutside = useCallback(
+    (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setOpen(false);
+      }
+    },
+    [ref, setOpen]
+  );
 
   useEffect(() => {
     if (open) {
@@ -28,16 +31,32 @@ export const FoldableUserLogo = () => {
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, [open]);
+  }, [open, handleClickOutside]);
+};
+
+export const FoldableUserLogo = () => {
+  const { user, logOutUser } = useAuth();
+  const [open, setOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null!);
+
+  useCloseWhenClickedOutside({ ref: menuRef, open, setOpen });
+  const handleLogout = () => {
+    logOutUser();
+    setOpen(false);
+  };
+
+  const toggleOpen = () => {
+    setOpen((prev) => !prev);
+  };
 
   if (!user) {
     return null;
   }
 
   return (
-    <div className="relative mx-2 flex items-center" ref={menuRef}>
+    <div className="relative mx-2 not-prose flex items-center" ref={menuRef}>
       <img
-        className="w-10 h-10 rounded-full cursor-pointer"
+        className="w-8 h-8 rounded-full cursor-pointer"
         src={assembleImageUrl(user.profilePicture)}
         alt="profile pic"
         onClick={toggleOpen}
@@ -45,15 +64,21 @@ export const FoldableUserLogo = () => {
       <AnimatePresence>
         {open && (
           <motion.div
-            initial={{ opacity: 0, y: -10 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -10 }}
+            initial={{ opacity: 0, scaleY: 0 }}
+            animate={{ opacity: 1, scaleY: 1 }}
+            exit={{ opacity: 0, scaleY: 0 }}
             transition={{ duration: 0.2 }}
-            className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-md shadow-lg z-20 overflow-hidden"
+            style={{ transformOrigin: "top" }}
+            className="absolute right-0 top-[152%] md:top-[132%] w-fit text-gray-700 bg-white border border-gray-200 rounded-md shadow-lg z-20 overflow-hidden"
           >
+            <div className="w-52 px-4 py-2">
+              <p>
+                Welcome <b>{user.username}</b>
+              </p>
+            </div>
             <button
-              onClick={logOutUser}
-              className="block w-full px-4 py-2 text-left text-gray-700 hover:bg-gray-100"
+              onClick={handleLogout}
+              className="block w-full px-4 py-2 hover:bg-gray-100"
             >
               Logout
             </button>
--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,9 +1,12 @@
 import { AnimatePresence, motion } from "framer-motion";
-import { forwardRef, useState } from "react";
+import { forwardRef, useRef, useState } from "react";
 import { FaBars, FaX } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
 import { useAuth } from "src/context/auth.context";
-import { FoldableUserLogo } from "../FoldableUserLogo";
+import {
+  FoldableUserLogo,
+  useCloseWhenClickedOutside,
+} from "../FoldableUserLogo";
 import { CustomNavLink } from "./CustomNavlink";
 
 const Navbar = () => {
@@ -26,9 +29,10 @@ const Navbar = () => {
 
   const links = (
     <>
+      <PreconfiguredNavlink to="/">Home</PreconfiguredNavlink>
+
       {user ? (
         <>
-          <PreconfiguredNavlink to="/">Home</PreconfiguredNavlink>
           <PreconfiguredNavlink to="/profile">Profile</PreconfiguredNavlink>
           <PreconfiguredNavlink to="/friends">Friends</PreconfiguredNavlink>
         </>
@@ -41,13 +45,20 @@ const Navbar = () => {
     </>
   );
 
+  const mobileMenuRef = useRef<HTMLDivElement>(null!);
+  useCloseWhenClickedOutside({
+    ref: mobileMenuRef,
+    open: isOpen,
+    setOpen: setIsOpen,
+  });
+
   return (
     <nav className="bg-gray-900 text-white shadow-lg w-screen px-4 sm:px-6 lg:px-8 absolute top-0 z-[1200]">
       <div className="flex items-center justify-between h-16">
         <NavLink to="/" className="text-white">
           <div className="flex items-center">
             <img
-              className="w-10 h-10 rounded-full"
+              className="w-8 h-8 rounded-full"
               src="/favicon/android-chrome-192x192.png"
               alt="logo favicon"
             />
@@ -57,13 +68,14 @@ const Navbar = () => {
 
         {/* Desktop Menu */}
         <div className="hidden md:flex space-x-4">
-          <FoldableUserLogo />
-
           <div className="flex items-center space-x-4">{links}</div>
+          <FoldableUserLogo />
         </div>
 
         {/* Mobile Menu Icon */}
         <div className="flex md:hidden">
+          <FoldableUserLogo />
+
           <button
             onClick={toggleMenu}
             className="text-white focus:outline-none"
@@ -77,13 +89,14 @@ const Navbar = () => {
       <AnimatePresence>
         {isOpen && (
           <motion.div
+            ref={mobileMenuRef}
             initial={{ height: 0 }}
             animate={{ height: "auto" }}
             exit={{ height: 0 }}
             className="md:hidden bg-gray-800 overflow-hidden z-10"
           >
-            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-              <FoldableUserLogo />
+            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 flex flex-col">
+              {/* <div className="self-end mr-2 mt-2"></div> */}
 
               {links}
             </div>
