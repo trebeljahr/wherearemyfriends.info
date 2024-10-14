5f41641f140a7b1d25efc1c58bc03ced09ad39f6
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2024-10-14 14:35:30 +0200

    edit svg marker

--- client/src/components/AvatarPinMarker.tsx
+++ client/src/components/AvatarPinMarker.tsx
@@ -1,3 +1,5 @@
+// AvatarPinMarker.tsx
+import React from "react";
 import { assembleImageUrl } from "../lib/consts";
 
 interface AvatarPinMarkerProps {
@@ -16,28 +18,38 @@ export const AvatarPinMarker: React.FC<AvatarPinMarkerProps> = ({
       width="50"
       height="70"
       viewBox="0 0 50 70"
-      fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      stroke="black"
     >
-      <defs>
-        <clipPath id="circleView">
-          <circle cx="25" cy="25" r="22" />
-        </clipPath>
-      </defs>
+      {/* Define the pin shape */}
       <path
-        d="M25 0C11.1929 0 0 11.1929 0 25C0 44 25 70 25 70C25 70 50 44 50 25C50 11.1929 38.8071 0 25 0Z"
+        d="
+          M25,0
+          C11.1929,0 0,11.1929 0,25
+          C0,39.641 25,70 25,70
+          C25,70 50,39.641 50,25
+          C50,11.1929 38.8071,0 25,0
+          Z
+        "
         fill={pinColor}
-        stroke={pinColor}
-        strokeWidth="2"
+        stroke="black"
+        strokeWidth={2}
       />
+      {/* Clip the image to the circle shape */}
+      <clipPath id="circleClip">
+        <circle cx="25" cy="25" r="25" />
+      </clipPath>
+      {/* Place the profile image */}
       <image
         href={imageUrl}
-        x="3"
-        y="3"
-        width="44"
-        height="44"
-        clipPath="url(#circleView)"
+        x="-20"
+        y="0"
+        width="100"
+        height="50"
+        clipPath="url(#circleClip)"
       />
+      {/* Add a circle border */}
+      <circle cx="25" cy="25" r="25" fill="none" />
     </svg>
   );
 };
